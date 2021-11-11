a199bd6a748e517a94e63ebf8e1138e703b8c4dc
Musharofchy <mosarrof121@gmail.com>
2021-11-11 20:00:16 +0600

    resolve fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
   flags: { PRESERVE_WEBPACK_CACHE: true },
   plugins: [
     {
-      resolve: `@uideck/docs-uideck`,
+      resolve: `@rocketseat/gatsby-theme-docs`,
       options: {
         configPath: `src/config`,
         docsPath: `src/docs`,
